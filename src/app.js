6ed48dcc2bf28ccb2aba0cc09265d561eecb31e7
Rudronha <rudronha@gmail.com>
2024-06-27 13:26:05 +0530

    add models that needed

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ const config = require('./config/config');
 const userRoutes = require('./routes/userRoutes');
 const session = require('express-session');
 const sequelize = require('./config/db');
+const User = require('./models/userModel');
+const Product = require('./models/ProductModel');
+const Transaction = require('./models/TransactionModel');
 
 const app = express();
-sequelize.sync();
+
+async function syncModels() {
+    try {
+      await sequelize.sync({ alter: true }); // Use alter: true to modify existing tables
+      console.log('Database synchronized successfully.');
+    } catch (error) {
+      console.error('Error synchronizing database:',error);
+    }
+} 
+syncModels();
+
 // Middleware for session management
 app.use(session({
     secret: 'secret', // Change this to a strong, random secret
